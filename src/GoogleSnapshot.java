9ecb462a4c00981f2320eb2a82d0e949ecabb777
Carrieukie <kariuki@gmail.com>
2020-11-06 21:44:40 +0300

    First commit

---
+++ src/GoogleSnapshot.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+public class GoogleSnapshot {
+    public static void main(String[] args) {
+        int[] array = {1, -10, 0, 10, 6, 8, -10};
+        System.out.println(solution(array));
+    }
+//[0,0,1,1,1,2,2,3,3,4]
+    public int removeDuplicates(int[] nums) {
+       int pointer1 = 0, pointer2 = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[pointer1] == nums[pointer2]){
+                   
+            }
+        }
+        return nums.length;
+    }
+
+    public static int solution(int[] A){
+        if (A.length <= 3){
+            return A.length;
+        }
+        Map<Integer, Integer> occurences = new HashMap<>();
+
+        for (int i = 0; i < A.length; i++) {
+           if (occurences.containsKey(A[i])){
+               occurences.put(A[i],occurences.get(A[i]) + 1);
+           }else {
+               occurences.put(A[i],1);
+           }
+        }
+        System.out.println(occurences);
+        int ans = 0;
+        for (int key : occurences.keySet()){
+            int startpointer = 0;
+            int endpointer = A.length -1;
+            //align pointers
+            while (true){
+                if (A[startpointer] == key && A[endpointer] == key){
+                    break;
+                }
+                if (!(A[startpointer] == key)){
+                    startpointer ++;
+                }
+                if (!(A[endpointer] == key)){
+                    endpointer--;
+                }
+
+            }
+            int arraysize = endpointer - startpointer + 1;
+            int localmax = arraysize - (arraysize - occurences.get(key) ) + 3;
+            if (localmax >= arraysize){
+                ans = Math.max(localmax,ans);
+            }
+        }
+        return  ans;
+    }
+
+    public static int solution1(String T) {
+        int validTimes = 1;
+        char[] cleanTime =  T.replace(":","").toCharArray();
+        for (int i = 0; i < cleanTime.length; i++) {
+            if (cleanTime[i] == '?' && i == 0){
+                validTimes *= 2;
+            }
+            if (cleanTime[i] == '?' && i == 1){
+                if (cleanTime[0] == '2'){
+                    validTimes *= 4;
+                }else {
+                    validTimes *= 10;
+                }
+
+            }
+            if (cleanTime[i] == '?' && i == 2){
+                validTimes *= 6;
+            }
+            if (cleanTime[i] == '?' && i == 3){
+                validTimes *= 10;
+            }
+        }
+        return validTimes == 1 ? 0 : validTimes;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer array[] = new Integer[]{5,4,3,6};
+        System.out.println(solution(array));
+        Integer loads[] = new Integer[]{1,2,3,4,5};
+        System.out.println(loads(loads));
+    }
+//    nums = [2,7,11,15], target = 9
+//    static int[] target(Integer[] nums, int target){
+//        int ans[] = new int[2];
+//        HashMap<Integer,Integer> complements = new HashMap<>();
+//        for (int i = 0; i < nums.length ; i++) {
+//            int complement = target - nums[i];
+//            if (complements.containsValue(complement)){
+//                ans[0] = i;
+//                ans[1] =
+//            }else {
+//                complements.put(i,complement);
+//            }
+//        }
+//
+//        return null;
+//    }
+
+    static int loads(Integer[] loads) {
+        int server1Time = 0;
+        int server2Time = 0;
+        int pointer1 = 0;
+        int pointer2 = loads.length -1;
+        while(pointer1 < pointer2){
+            if (server1Time < server2Time){
+                server1Time += loads[pointer1];
+                pointer1++;
+            }else {
+                server2Time += loads[pointer2];
+                pointer2--;
+            }
+        }
+        return Math.abs(server1Time - server2Time);
+    }
+
+    static int solution(Integer[] A) {
+        int ans = 0;
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] > largest){
+                largest = A[i];
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
